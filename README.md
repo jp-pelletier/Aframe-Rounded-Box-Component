585fbb1d1b2fbc2fbc888aa8dba9b4dc95a3dc20
jp-pelletier <jp.pelletier@travail-social.com>
2022-07-10 16:14:12 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,45 @@
+# THREE.JS - Aframe Rounded Angles Box Component
+
+Light component for some rounded effects on A-FRAME boxes.
+
+Click the cover below for a live demo... and look behind !
+
+<a href="https://jp-pelletier.github.io/" target="_blank"><img src="https://github.com/jp-pelletier/Aframe-Rounded-Box-Component/blob/master/cover/repository-open-graph-template.jpg"></a>
+
+## Usage
+
+Example of code of the Wooden Cube :
+```yaml
+<a-entity
+    roundedanglesbox = "
+    width: 1;
+    height: 1;
+    depth: 1;
+    radius: 0.2;
+    roughness: 0;
+    metalness: 0;
+    src: img/Wood.jpg;
+    repeat: 4 4;"
+    position = "-4 0.6 -0.5"
+    rotation = "0 40 0">
+</a-entity>
+```
+
+
+ PROPERTY | COMPONENT MAPPING <BR> DESCRIPTION | DEFAULT VALUE |
+--- | --- | --- |
+Width | geometry.width | 1 |
+Height | geometry.height | 1 |
+Depth | geometry.depth | 1 |
+Radius | Determines the radius of the rounded angles.<br>Value "1" produces a sphere for an equirectangular box. | 0.05 |
+Segments | Determines the number of vertices for THREE.BoxBufferGeometry segments x, y, z. | 9 9 9 |
+Color | material.color | #AAA |
+Roughness | material.roughness<br>How rough the material is from 0 to 1. A rougher material will scatter reflected light in more directions than a smooth material. | 0 |
+Metalness | material.metalness<br>How metallic the material is from 0 to 1. | 0.2 |
+Transparent | Whether material is transparent. Transparent entities are rendered after non-transparent entities. | false |
+Opacity | Extent of transparency. If the transparent property is not true, then the material will remain opaque and opacity will only affect color. | 1 |
+Src | material.src<br>Image texture map. Requires an inline URL.<br>This simple texture map produces a very cool effect on edges ! | None |
+Repeat | material.repeat<br>How many times a texture (defined by src) repeats in the X and Y direction. | 1 1 |
+cubeMap | Unlike textures, the envMap property takes a cubemap, six images put together to form a cube. The cubemap wraps around the mesh and applied as a texture.<br>Requires six images loaded with parameters :<br>cubeMap_negx<br>cubeMap_posz<br>cubeMap_posx<br>cubeMap_negz<br>cubeMap_posy<br>cubeMap_negy<br><br>Textures directory has to be configured in roundedanglesbox.js.<br><br>Using cubeMap switches the material from MeshStandardMaterial to MeshPhongMaterial and doesn't support metalness and roughness. | None |
+
+For porting @Fyrestar’s approach to JavaScript, look here : https://discourse.threejs.org/t/cheap-round-edged-box-vertex-shader/8066/21
---
+++ js/roundedanglesbox.js
@@ -0,0 +1,106 @@
+//*********************************************
+// AFRAME ROUNDED ANGLES BOX LIGHT COMPONENT
+//*********************************************
+
+AFRAME.registerComponent('roundedanglesbox', {
+    schema:
+    {
+        width:        { type: 'number',  default: 1 },
+        height:       { type: 'number',  default: 1 },
+        depth:        { type: 'number',  default: 1 },
+        radius:       { type: 'number',  default: 0.05 },
+        segments:     { type: 'vec3',    default: { x: 9, y: 9, z: 9 } },
+        color:        { type: 'color',   default: '#AAA' },
+        roughness:    { type: 'number',  default: 0 },
+        metalness:    { type: 'number',  default: 0.2 },
+        transparent:  { type: 'boolean', default: false },
+        opacity:      { type: 'number',  default: 1 },
+        src:          { type: 'asset',   default: '' },
+        repeat:       { type: 'vec2',    default: { x: 1, y: 1 } },
+        cubeMap_negx: { type: 'asset',   default: '' },
+        cubeMap_posz: { type: 'asset',   default: '' },
+        cubeMap_posx: { type: 'asset',   default: '' },
+        cubeMap_negz: { type: 'asset',   default: '' },
+        cubeMap_posy: { type: 'asset',   default: '' },
+        cubeMap_negy: { type: 'asset',   default: '' }
+    },
+
+    init: function () {
+        var data = this.data;
+        var el = this.el;
+
+        this.geometry = new THREE.BoxBufferGeometry(1, 1, 1, data.segments.x, data.segments.y, data.segments.z);
+
+        this.material = new THREE.MeshStandardMaterial({
+            color: data.color,
+            roughness: data.roughness,
+            metalness: data.metalness,
+            transparent: data.transparent,
+            opacity: data.opacity,
+        });
+
+        if (data.src != '') {
+            const textureBox = new THREE.TextureLoader().load(data.src);
+            textureBox.wrapS = THREE.RepeatWrapping;
+            textureBox.wrapT = THREE.RepeatWrapping;
+            textureBox.repeat = data.repeat;
+            this.material.map = textureBox;
+        }
+
+        if (data.cubeMap_negx != '' && data.cubeMap_posx != '' && data.cubeMap_negy != '' && data.cubeMap_posy != '' && data.cubeMap_negz != '' && data.cubeMap_posz != '') {
+            this.material = new THREE.MeshPhongMaterial({
+                color: data.color,
+                transparent: data.transparent,
+                opacity: data.opacity,
+            });
+            const loader = new THREE.CubeTextureLoader();
+            loader.setCrossOrigin("");
+            loader.setPath('img/textures/');
+
+            var cubeTexture = loader.load([
+                data.cubeMap_negx, data.cubeMap_posz,
+                data.cubeMap_posx, data.cubeMap_negz,
+                data.cubeMap_posy, data.cubeMap_negy
+            ]);
+            this.material.envMap = cubeTexture
+        }
+
+        var boxMat = this.material;
+
+        const settings = {
+            radius: { value: data.radius },
+            size: {
+                value: new THREE.Vector3(data.width, data.height, data.depth)
+            }
+        }
+
+        boxMat.onBeforeCompile = shader => {
+
+            shader.uniforms.boxSize = settings.size;
+            shader.uniforms.radius = settings.radius;
+
+            shader.vertexShader = `
+            uniform vec3 boxSize;
+            uniform float radius;
+            ` + shader.vertexShader;
+
+            shader.vertexShader = shader.vertexShader.replace(
+            `#include <begin_vertex>`,
+            `#include <begin_vertex>
+
+            vec3 signs = sign(position);
+            vec3 box = boxSize - vec3(radius);
+            box = vec3(max(0.0, box.x), max(0.0, box.y), max(0.0, box.z));
+            vec3 p = signs * box;
+  
+            transformed = signs * box * 0.5 + normalize(position) * radius;
+      
+            objectNormal = all(equal(p, transformed)) ? normal : normalize(position); 
+            transformedNormal = normalize(normalMatrix * objectNormal);
+            vNormal = transformedNormal;`
+            );
+        };
+        this.mesh = new THREE.Mesh(this.geometry, boxMat);
+        el.setObject3D('mesh', this.mesh);
+    }
+});
